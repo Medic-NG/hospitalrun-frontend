8f5dc3bd072ec42b502cbbf59b8b61fe66ab9c01
Austine Ikenna Aku <ikeyy2000@gmail.com>
2019-02-06 10:18:59 +0100

    delete data

--- tests/helpers/typeahead-fillin.js
+++
@@ -1,8 +0,0 @@
-async function typeAheadFillIn(selector, value) {
-  let typeAheadSelector = `${selector} .tt-input`;
-  await fillIn(typeAheadSelector, value);
-  await triggerEvent(typeAheadSelector, 'input');
-  await triggerEvent(typeAheadSelector, 'blur');
-}
-
-export default typeAheadFillIn;
